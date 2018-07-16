a0e65e6435465c3d21ce80ad82b19fe68672148c
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2018-07-16 08:10:46 +0200

    update_bcd.js: seems to be working

--- update_bcd.js
+++ update_bcd.js
@@ -22,7 +22,88 @@ Example:
   return;
 }
 
-const tree = utils.readJsonSync(nctFile);
+const jsDir = path.join(bcdDir, 'javascript/');
 
 const map = utils.readJsonSync(bcdFile);
 
+function get(tree, ...path) {
+  const first = path.shift();
+  return tree === undefined ? undefined : path.length === 0 ? tree[first] : get(tree[first], ...path);
+}
+
+function feq(f1, f2) {
+  if (f1 === undefined && f2 === undefined) return true;
+  if (! Array.isArray(f1) || ! Array.isArray(f2)) return false;
+  // TODO: handle multiple flags if necessary
+  if (f1.length !== 1 || f1.length !== f2.length) return false;
+  if (f1[0].type !== f2[0].type) return false;
+  if (f1[0].name !== f2[0].name) return false;
+  return true;
+}
+
+function update(bpath, flag, versions) {
+  if (map[bpath] === undefined) throw new Error('invalid BCD path ' + bpath);
+  const file = path.join(jsDir, map[bpath].bcd_file);
+  const tree = utils.readJsonSync(file);
+  const support = get(tree, 'javascript', ...bpath.split('/'), '__compat', 'support');
+  if (support === undefined) throw new Error('invalid BCD path ' + bpath);
+
+  const nodejs = support.nodejs;
+  if (! nodejs) {
+    support.nodejs = versions;
+  }
+  else if (Array.isArray(nodejs)) {
+    let found = false;
+    for (const i in nodejs) {
+      if (feq(nodejs[i].flags, versions.flags)) {
+        nodejs[i] = versions;
+        found = true;
+      }
+    }
+    if (! found) nodejs.push(versions);
+  }
+  else if (feq(nodejs.flags, versions.flags)) {
+    support.nodejs = versions;
+  }
+  else {
+    support.nodejs = [nodejs, versions];
+  }
+
+  utils.writeJsonSync(file, tree);
+}
+
+const flags = ['', '--harmony'];
+
+function updatePath(tree, path) {
+  for (const flag of flags) {
+    if (tree[flag]) {
+      const versions = tree[flag];
+      if (flag !== '') versions.flags = [ { type: 'runtime_flag', name: flag } ];
+      update(path, flag, versions);
+    }
+  }
+}
+
+function updateAll(tree) {
+  if (tree.bcd_path !== undefined) {
+    if (tree.bcd_path === '') {
+      return;
+    }
+    else if (Array.isArray(tree.bcd_path)) {
+      for (const path of tree.bcd_path) {
+        updatePath(tree, path);
+      }
+    }
+    else {
+      updatePath(tree, tree.bcd_path);
+    }
+  }
+  else {
+    for (const child of Object.values(tree)) {
+      updateAll(child);
+    }
+  }
+}
+
+const tree = utils.readJsonSync(nctFile);
+updateAll(tree);
