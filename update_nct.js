69a85ec853b76e32f211d057d6936b8c5798cdb8
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2018-07-17 21:53:53 +0200

    update_nct.js: refactoring, comments

--- update_nct.js
+++ update_nct.js
@@ -21,6 +21,7 @@ Example:
   return;
 }
 
+// recursively iterate through version data nodes
 function *leafs(tree) {
   if (tree.bcd_path !== undefined) {
     yield tree;
@@ -32,20 +33,23 @@ function *leafs(tree) {
   }
 }
 
-function varr(v, c = 3) {
+// split version string into array with given number of parts
+function varr(v, n) {
   let a = v.split('.');
-  while (a.length < c) a.push(0);
-  while (a.length > c) a.pop();
+  while (a.length < n) a.push(0);
+  while (a.length > n) a.pop();
   return a.map(n => parseInt(n));
 }
 
-function vclean(v, c = 3) {
-  return varr(v, c).join('.');
+// rebuild version string with given number of parts
+function vclean(v, n) {
+  return varr(v, n).join('.');
 }
 
-function vcmp(v1, v2, c = 3) {
-  const a1 = varr(v1, c);
-  const a2 = varr(v2, c);
+// compare given number of parts of version strings
+function vcmp(v1, v2, n) {
+  const a1 = varr(v1, n);
+  const a2 = varr(v2, n);
   for (const i in a1) {
     if (a1[i] < a2[i]) return -1;
     if (a1[i] > a2[i]) return +1;
@@ -56,97 +60,103 @@ function vcmp(v1, v2, c = 3) {
 const flags = ['', '--harmony'];
 const bounds = ['version_added', 'version_removed'];
 
-const v8dir = path.join(nctDir, 'results/v8/');
-
+// main data tree
 const tree = utils.readJsonSync(nctFile);
 
-const files = Object.create(null);
+const v8dir = path.join(nctDir, 'results/v8/');
 
+// load v8/*.json file
+function nctJson(name) {
+  const file = path.join(v8dir, name);
+  return utils.readJsonSync(file);
+}
+
+// build version -> file map
+const files = Object.create(null);
 for (const name of fs.readdirSync(v8dir)) {
   const match = /([0-9]+\.[0-9]+\.[0-9]+|nightly)(--harmony)?\.json/.exec(name);
   if (match) {
     let ver = match[1];
     const flag = match[2] === undefined ? '' : match[2];
-
     if (ver == 'nightly') {
-      const file = path.join(v8dir, name);
-      const nct = utils.readJsonSync(file);
-      ver = vclean(nct._version);
+      const data = nctJson(name);
+      ver = vclean(data._version, 3);
     }
-
     if (! files[ver]) files[ver] = Object.create(null);
     files[ver][flag] = name;
   }
 }
 
 const versions = Object.keys(files);
+versions.sort((v1, v2) => vcmp(v1, v2, 3));
 
-versions.sort(vcmp);
-
-// reset data
+// clear version nodes in tree
 for (const node of leafs(tree)) {
-  for (const flag of flags) node[flag] = Object.create(null);
+  for (const flag of flags) {
+    node[flag] = Object.create(null);
+  }
 }
 
-function vset(tree, ver, ok, ...path) {
+// find node in tree
+function *find(tree, ...path) {
   const first = path.shift();
   const node = tree[first];
   if (node === undefined) return;
-  if (path.length === 0) {
-    if (ok) {
-      if (! node.version_added) {
-        node.version_added = ver;
-      }
-      else if (node.version_added && node.version_removed) {
-        // Feature was added, removed, added again. Keep only latest data.
-        node.version_added = ver;
-        delete node.version_removed;
-      }
+  if (path.length === 0) yield node;
+  else yield *find(node, ...path);
+}
+
+// update version data in node if necessary
+// Note: this only works when called with versions in ascending order!
+function vset(node, ver, ok) {
+  if (ok) {
+    if (! node.version_added) {
+      // Feature was added in this version
+      node.version_added = ver;
     }
-    else {
-      if (node.version_added && ! node.version_removed) {
-        node.version_removed = ver;
-      }
+    else if (node.version_added && node.version_removed) {
+      // Feature was added, removed, now added again. Keep only latest data.
+      node.version_added = ver;
+      delete node.version_removed;
     }
   }
   else {
-    vset(node, ver, ok, ...path);
+    if (node.version_added && ! node.version_removed) {
+      // Feature was added in this version
+      node.version_removed = ver;
+    }
   }
 }
 
+// read version data from files, update version tree
 for (const ver of versions) {
   for (const flag of flags) {
     const name = files[ver][flag];
-
     // there's no --harmony file for some 0.12.x versions
     if (name === undefined && ver.startsWith('0.12.')) continue;
-
-    const file = path.join(v8dir, name);
-    const nct = utils.readJsonSync(file);
+    const nct = nctJson(name);
     for (const [tag, data] of Object.entries(nct)) {
-
-      // skip _version, _engine
-      if (tag.startsWith('_')) continue;
-
+      if (tag.startsWith('_')) continue; // skip _version, _engine
       for (const [key, val] of Object.entries(data)) {
-
-        // skip _successful, _count, _percent
-        if (key.startsWith('_')) continue;
-
+        if (key.startsWith('_')) continue; // skip _successful, _count, _percent
         const parts = key.split('›');
-        vset(tree, ver, val === true, tag, ...parts, flag);
+        for (const node of find(tree, tag, ...parts, flag)) {
+          vset(node, ver, val === true);
+        }
       }
     }
   }
 }
 
-function veq(d1, d2) {
+// do nodes contain equal version data?
+function veq(n1, n2) {
   for (const bound of bounds) {
-    if (d1[bound] !== d2[bound]) return false;
+    if (n1[bound] !== n2[bound]) return false;
   }
   return true;
 }
 
+// clean up data
 for (const node of leafs(tree)) {
 
   // remove patch version from 0.x versions - too many changes
